9ea4c2fd47fe096326288432a9edb55d5d750d98
Banly <banlysrb@gmail.com>
2019-08-15 22:48:03 +0700

    design la layout for about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
 
 const About = () => (
-  <div>About</div>
+  <div className="container mx-auto">
+    <h1 className="text-5xl text-center py-5">Thinkers,Designers, make great things happen</h1>
+    <div className="photos flex">
+      <div className="px-5">
+        <img src="https://insights.dice.com/wp-content/uploads/2018/03/Hipster-Developer-Dice.jpg" alt="" />
+      </div>
+      <div className="px-5">
+        <img src="https://insights.dice.com/wp-content/uploads/2018/03/Hipster-Developer-Dice.jpg" alt="" />
+      </div>
+      <div className="px-5">
+        <img src="https://insights.dice.com/wp-content/uploads/2018/03/Hipster-Developer-Dice.jpg" alt="" />
+      </div>
+      <div className="px-5">
+        <img src="https://insights.dice.com/wp-content/uploads/2018/03/Hipster-Developer-Dice.jpg" alt="" />
+      </div>
+    </div>
+    <div className="message my-5">
+      <h2 className="text-4xl text-center">What we believe!</h2>
+    </div>
+    <div className="content">
+      <div className="flex justify-around">
+        <div className="px-5">
+          <h3 className="text-2xl">People come first</h3>
+          <p className="ml-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facere asperiores libero voluptatum dicta, eaque. Dignissimos cumque, esse voluptates, explicabo nam voluptatum itaque aliquid delectus suscipit doloribus hic sed. Fuga rem ad, nulla quos ratione veniam repudiandae sequi est in aperiam cum quam obcaecati sapiente nesciunt dicta, soluta molestias veritatis odit accusantium eligendi ipsum nam harum praesentium ab. Rem, veniam ea ipsam! Porro adipisci dolor suscipit, ipsa quasi voluptatum repellendus necessitatibus totam a, aspernatur distinctio minima deserunt cumque in sint ducimus eveniet molestiae alias temporibus maxime deleniti explicabo. Quibusdam placeat at, dolorem eligendi eum, obcaecati ipsa rem laboriosam explicabo eveniet.</p>
+        </div>
+        <div className="px-5">
+          <h3 className="text-2xl">Less is better</h3>
+          <p className="ml-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque facere asperiores libero voluptatum dicta, eaque. Dignissimos cumque, esse voluptates, explicabo nam voluptatum itaque aliquid delectus suscipit doloribus hic sed. Fuga rem ad, nulla quos ratione veniam repudiandae sequi est in aperiam cum quam obcaecati sapiente nesciunt dicta, soluta molestias veritatis odit accusantium eligendi ipsum nam harum praesentium ab. Rem, veniam ea ipsam! Porro adipisci dolor suscipit, ipsa quasi voluptatum repellendus necessitatibus totam a, aspernatur distinctio minima deserunt cumque in sint ducimus eveniet molestiae alias temporibus maxime deleniti explicabo. Quibusdam placeat at, dolorem eligendi eum, obcaecati ipsa rem laboriosam explicabo eveniet.</p>
+        </div>
+      </div>
+    </div>
+    <div className="conclude">
+      <h3 className="text-2xl">Create positive change</h3>
+      <p className="ml-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div className="tools px-32 my-5">
+      <h2 className="text-4xl text-center">Our tools</h2>
+    <div className="flex justify-center items-center">
+        <div className="flex-1 px-5">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="" />
+        </div>
+        <div className="flex-1 px-5">
+          <img src="https://expressjs.com/images/express-facebook-share.png" alt="" />
+        </div>
+        <div className="flex-1 px-5">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png" alt="" />
+        </div>
+        <div className="flex-1 px-5">
+          <img src="https://webassets.mongodb.com/_com_assets/cms/mongodb_logo1-76twgcu2dm.png" alt="" />
+        </div>
+        <div className="flex-1 px-5">
+          <img src="https://vuejs.org/images/logo.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <div className="help">
+    </div>
+  </div>
 );
 
 export default About;
