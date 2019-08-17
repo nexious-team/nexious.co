e256d9d95fd601247066096a1e673ceb55d8b3f3
Banly <banlysrb@gmail.com>
2019-08-17 16:25:02 +0700

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const About = () => (
-  <div className="container mx-auto">
-    <h1 className="text-5xl text-center py-5">Thinkers,Designers, make great things happen</h1>
+  <div className="">
+    <div className="relative">
+      <img className="w-full h-96" src="https://images.pexels.com/photos/1661004/pexels-photo-1661004.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
+    <div className="absolute inset-0">
+      <h1 className="text-5xl text-center py-5">Thinkers,Designers, make great things happen</h1>
+    </div>
+    </div>
     <div className="photos flex">
       <div className="px-5">
         <img src="https://insights.dice.com/wp-content/uploads/2018/03/Hipster-Developer-Dice.jpg" alt="" />
@@ -36,9 +41,9 @@ const About = () => (
       <h3 className="text-2xl">Create positive change</h3>
       <p className="ml-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-    <div className="tools px-32 my-5">
+    <div className="tools my-5">
       <h2 className="text-4xl text-center">Our tools</h2>
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center px-32">
         <div className="flex-1 px-5">
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="" />
         </div>
@@ -52,9 +57,24 @@ const About = () => (
           <img src="https://webassets.mongodb.com/_com_assets/cms/mongodb_logo1-76twgcu2dm.png" alt="" />
         </div>
         <div className="flex-1 px-5">
-          <img src="https://vuejs.org/images/logo.png" alt="" />
+          <img className="w-32" src="https://vuejs.org/images/logo.png" alt="" />
         </div>
       </div>
+      <div className="flex justify-center items-center px-32">
+        <div className="flex-1 px-5">
+        </div>
+          <div className="flex-1 px-5">
+            <img src="https://www.b2bnn.com/wp-content/uploads/2019/01/Screen-Shot-2019-01-17-at-2.29.34-PM.png" alt="" />
+          </div>
+          <div className="flex-1 px-5">
+            <img className="w-32" src="https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png" alt="" />
+          </div>
+          <div className="flex-1 px-5">
+            <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/u/res/images/brand-assets/Logos/0099ec3754bf473d2bbf317204ab6fea/trello-logo-blue.png" alt="" />
+          </div>
+          <div className="flex-1 px-5">
+          </div>
+        </div>
     </div>
     <div className="help">
     </div>
