83052098ed3b30a6e2f9356fb22290c4ad4d9047
BanlyTong <banly2602@gmail.com>
2019-08-29 20:28:00 +0700

    create small components

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem',
+        '120': '30rem',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
