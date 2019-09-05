630af58337f114679c24d7d8d057021805abbb8c
kimthunbunly <kimthunbunly.rupp@gmail.com>
2019-09-05 21:59:32 +0700

    add get-start-form into project

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import Home from './Home'
 import Service from './Service'
 import About from './About'
 import Client from './Client'
+import GetStart from './GetStart'
 
 export {
   Home,
   Service,
   About,
-  Client
+  Client,
+  GetStart
 }
