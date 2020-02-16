549bb968c90952a95840ed21acfd9bd229675bde
BanlyTong <banly2602@gmail.com>
2020-02-16 13:14:41 +0700

    translate service page

--- src/translations/en/index.js
+++ src/translations/en/index.js
@@ -3,5 +3,6 @@ import home from './home.json'
 import contact from './contact.json'
 import about from './about.json'
 import footer from './footer.json'
+import service from './service.json'
 
-export default { ...app, ...home, ...contact, ...about, ...footer }
+export default { ...app, ...home, ...contact, ...about, ...footer, ...service }
