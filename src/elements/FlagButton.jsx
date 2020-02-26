9e7add8815238406df7852bccfd153ccb7e218b9
BanlyTong <banly2602@gmail.com>
2020-02-26 21:31:24 +0700

    change dropdown to flag button

---
+++ src/elements/FlagButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default ({ lang, onClickFlag }) => {
+  
+  const flagName = lang === 'Km' ? 'united-states-of-america-flag-icon-32' : 'cambodia-flag-icon-32';
+
+  return (
+    <button className="my-auto h-4 w-6" onClick={onClickFlag}>
+      {<img className="object-cover h-full w-full" src={`/img/${flagName}.png`} alt="Flag"/>}
+    </button>
+  )
+}
