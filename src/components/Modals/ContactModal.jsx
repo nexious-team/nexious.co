40033fbe68457d0cb10fb9e093ed9b3e833fed6f
banlymbd <banly@mekongbigdata.com>
2022-04-02 13:51:32 +0700

    fix: strink modal on mobile

--- src/components/Modals/ContactModal.jsx
+++ src/components/Modals/ContactModal.jsx
@@ -43,9 +43,9 @@ export default function ContactModal({ open, setOpen }) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-0 sm:align-middle sm:max-w-lg w-full sm:w-full">
               <div className="bg-white px-4 py-8 sm:p-8 sm:pb-8 flex justify-center">
-                <div className="w-2/4">
+                <div className="w-3/4 lg:w-2/4">
                   <div>
                     <ContactItem
                       icon={"mobile-alt"}
